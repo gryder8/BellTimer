fe74a85027f2224c425681b296653245681563dc
Gavin Ryder <36829208+gryder8@users.noreply.github.com>
2019-01-16 21:29:02 -0800

    Init

---
+++ Bell View/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Bell View
+//
+//  Created by Gavin Ryder on 1/11/19.
+//  Copyright © 2019 Gavin Ryder. All rights reserved.
+//
+
+import UIKit
+import WebKit
+class ViewController: UIViewController, WKUIDelegate {
+
+//    var webView: WKWebView!
+//
+//
+//    override func loadView() {
+//        let webConfiguration = WKWebViewConfiguration()
+//        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+//        webView.uiDelegate = self
+//        view = webView
+//    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        let myURL = URL(string:"https://bell.plus/lahs")
+//        let myRequest = URLRequest(url: myURL!)
+//        webView.load(myRequest)
+//    }
+}
+
