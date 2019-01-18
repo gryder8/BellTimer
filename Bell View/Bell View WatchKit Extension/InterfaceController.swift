6bffcc031980563b160f2048c9beaf8e26632047
Gavin Ryder <36829208+gryder8@users.noreply.github.com>
2019-01-17 21:58:00 -0800

    Changed progress bar and cleaned UI
    Added the circular color bar to the project, cleaned up colors
    
    Color ring courtesy: https://github.com/luispadron/uicircularprogressring

---
+++ Bell View/Bell View WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,30 @@
+//
+//  InterfaceController.swift
+//  Bell View WatchKit Extension
+//
+//  Created by Gavin Ryder on 1/11/19.
+//  Copyright © 2019 Gavin Ryder. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
