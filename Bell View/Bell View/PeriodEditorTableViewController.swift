41ab063a730d40e3a3ce212daf9038258478fde0
gryder8 <gavin.ryder82@gmail.com>
2020-01-21 16:04:05 -0800

    Cleaned up

--- Bell View/Bell View/PeriodEditorTableViewController.swift
+++ Bell View/Bell View/PeriodEditorTableViewController.swift
@@ -153,18 +153,6 @@ class PeriodEditorTableViewController: UITableViewController {
     
 
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
